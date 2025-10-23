d4357c2796b2a3818d3baec5bd626989312ce9ff
Maekiee <pdwssf@gmail.com>
2025-10-23 15:34:45 +0900

    Initial Commit

---
+++ PracticeSwiftUI/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  PracticeSwiftUI
+//
+//  Created by 박도원 on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var foodCount = 0
+    @State private var waterCount = 0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("밥알: \(foodCount)개")
+                Text("물방울: \(waterCount)개")
+            }
+            CustomButton(title: "밥먹기", count: $foodCount)
+            CustomButton(title: "물먹기", count: $waterCount)
+        }
+    }
+}
+
+struct CustomButton: View {
+    var title: String
+    @Binding var count: Int
+    
+    var body: some View {
+        Button(title) {
+            count += 1
+        }
+        .padding()
+        .border(.gray, width: 1)
+        
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by 박도원 on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
